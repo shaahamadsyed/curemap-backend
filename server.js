daec3c632095d57181bc7144bc0680aa18516d34
shaahamad-syed <shaahamadsyed@gmail.com>
2025-12-16 00:12:48 +0530

    Add /health route for Render health check

--- server.js
+++ server.js
@@ -27,5 +27,10 @@ app.get("/", (req, res) => {
     res.send("CureMap Backend Running...");
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
